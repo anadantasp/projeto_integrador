b0cfcd8b93dadf5046389789714d458b96c14d8b
luan998 <luanrodrigues998@outlook.com>
2021-11-04 11:04:37 -0300

    Projeto Integrador

---
+++ BackEnd/desembalamenos/src/main/java/br/org/generation/desembalamenos/DesembalamenosApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.desembalamenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesembalamenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesembalamenosApplication.class, args);
+	}
+
+}
---
+++ BackEnd/desembalamenos/src/main/java/br/org/generation/desembalamenos/model/Categoria.java
@@ -0,0 +1,77 @@
+package br.org.generation.desembalamenos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_categoria")
+public class Categoria {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "O nome da categoria é obrigatório!")
+	@Size(min = 4, max = 255, message = "A categoria deve conter mais que 4 carácteres e menos que 255 carácteres.")
+	private String categoria;
+	
+	
+	@NotNull(message = "O campo da descrição é obrigatório!")
+	@Size(min = 5, max = 1000, message = "A descrição deve conter mais que 5 carácteres e menos que 1000 carácteres.")
+	private String descricao;
+	
+	
+	@NotNull(message = "O nome da palavra chave é obrigatório!")
+	@Size(min = 4, max = 255, message = "A palavra chave deve conter mais que 4 carácteres e menos que 255 carácteres.")
+	private String palavraChave;
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getPalavraChave() {
+		return palavraChave;
+	}
+
+
+	public void setPalavraChave(String palavraChave) {
+		this.palavraChave = palavraChave;
+	}
+	
+	
+	
+	
+}
