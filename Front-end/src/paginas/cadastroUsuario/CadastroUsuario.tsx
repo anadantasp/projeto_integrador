c852cd29db808fb35f68b7dc8799338e5be61a50
anadantasp <caroldantasp@gmail.com>
2021-12-02 10:37:45 -0300

    Criação do projeto react desembalamenos

---
+++ Front-end/src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CadastroUsuario.css';
+
+function CadastroUsuario() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CadastroUsuario
